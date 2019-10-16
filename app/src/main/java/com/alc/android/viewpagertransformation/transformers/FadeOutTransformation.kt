9d9b49c8202885567561a3a02b75e584ff29c708
Maduoma <aachilefu@yahoo.com>
2019-10-16 18:31:49 +0100

    Initial commit

---
+++ app/src/main/java/com/alc/android/viewpagertransformation/transformers/FadeOutTransformation.kt
@@ -0,0 +1,13 @@
+package com.alc.android.viewpagertransformation.transformers
+
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+
+class FadeOutTransformation : ViewPager.PageTransformer {
+
+    override fun transformPage(page: View, position: Float) {
+
+        page.translationX = -position * page.width
+        page.alpha = 1 - Math.abs(position)
+    }
+}
